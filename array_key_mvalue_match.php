e530df38ebd5bffd2c5ab640f7005210bd1b0d3a
Ranjo8192 <34912190+ranjo8192@users.noreply.github.com>
2018-10-15 19:35:53 +0530

    Add files via upload

--- array_key_mvalue_match.php
+++ array_key_mvalue_match.php
@@ -1,24 +1,25 @@
 <?php
 $array =  array(
 		"State Labor Law Posters" => "Alabama",
-
         "Alabama" => "Kits",
-
         "Home" => "Kits Only"
 	);
 	echo "<pre>";
 	print_r($array);
-	$keys = array_keys($array); echo "<pre>"; print_r($keys);
-	$values = array_values($array); echo "<pre>"; print_r($values);
+	$keys = array_keys($array); 
+	$values = array_values($array); 
 	$data = NULL;
 	foreach($array as $key => $value)
 	{
 		
 		if($data == $key)
 		{
-			$url = $key."/".$value."/".$key;
+			$keys = array_search($key,$array);
+			echo $keys;
+			$url = $keys."/".$key."/".$array[$key];
+			echo $url; 
 		}
 		$data = $value;
 	}
-	print_r($url);
+	//print_r($url);
 ?>
